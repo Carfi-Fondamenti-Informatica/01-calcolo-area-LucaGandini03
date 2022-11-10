1b5e62ae932bd03d34d386e3b94fcd825a2e9018
LucaGandini03 <116078651+LucaGandini03@users.noreply.github.com>
2022-11-10 22:53:50 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
-using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
+int main() {
+    //inizializzo le variabili di input
+   float a=0;
+   float b=0;
+   float c=0;
+   //fornisco i valori alle relative variabili
+    std::cout<<"valore di a: ";std::cin>>a;
+    std::cout<<"valore di b: ";std::cin>>b;
+    std::cout<<"valore di c: ";std::cin>>c;
+    //inizializzo le variabili di output
+    float ATriangolo=0;
+    float AQuadrato=0;
+    float ARettangolo=0;
+    float ATrapezio=0;
+   //assegno ad ogni variabile l'operazione necessaria per ottenere il risultato chiesto
+   ATriangolo=(a*b)/2;
+   AQuadrato=a*a;
+   ARettangolo=a*b;
+   ATrapezio=((a+b)*c)/2;
+   //stampo a video i risultati delle varie operazioni
+   std::cout<<"Area del triangolo: "<<ATriangolo<<std::endl;
+   std::cout<<"Area del quadrato: "<<AQuadrato<<std::endl;
+   std::cout<<"Area del rettangolo: "<<ARettangolo<<std::endl;
+   std::cout<<"Area del trapezio: "<<ATrapezio<<std::endl;
 
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    return 0;
 }
